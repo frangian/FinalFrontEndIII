dd8280f6553c89cae5a57d9509f679c226f7d370
frangian <franciscogianzanti@gmail.com>
2022-12-09 02:29:47 -0300

    ajustes

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import { useState, useEffect } from "react";
 import Card from '../Components/Card'
+import axios from "axios";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Home = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users")
+      .then(res => {
+        setData(res.data);
+      }
+      )
+  }, [])
+
+
   return (
-    <main className="" >
+    <>
       <h1>Home</h1>
-      <div className='card-grid'>
-        {/* Aqui deberias renderizar las cards */}
+      <div className="card-grid container">
+      {data.map(resp => <Card key={resp.id} name={resp.name} username={resp.username} id={resp.id} />)}
       </div>
-    </main>
+    </>
   )
 }
 
