e7078fb550c7c6ced0601160cc9cf65eccc90f21
frangian <franciscogianzanti@gmail.com>
2022-12-09 02:13:29 -0300

    favoritos

---
+++ src/Components/utils/reducer.service.js
@@ -0,0 +1,19 @@
+export const initialState = { theme: "light", login: !!localStorage.getItem("token") };
+
+export const actions = {
+    SET_THEME_LIGHT: "SET_THEME_LIGHT",
+    SET_THEME_DARK: "SET_THEME_DARK",
+    SET_LOGIN: "SET_LOGIN",
+    SET_LOGOUT: "SET_LOGOUT"
+}
+
+export const reducer = (state, action) =>{
+    switch (action.type) {
+        case actions.SET_THEME_DARK:
+            return ({...state, theme: "dark"})
+        case actions.SET_THEME_LIGHT:
+            return  ({...state, theme: "light"})
+               default: {
+            return state}
+    }
+}
--- src/Routes/Detail.jsx
+++ src/Routes/Detail.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import DetailCard from "../Components/DetailCard";
 
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
@@ -9,9 +10,7 @@ const Detail = () => {
 
   return (
     <>
-      <h1>Detail Dentist id </h1>
-      {/* aqui deberan renderizar la informacion en detalle de un user en especifico */}
-      {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
+      <DetailCard />
     </>
   )
 }
