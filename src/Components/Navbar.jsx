a3c5d03b2b43baa0327234afda4e92256c0dfaa2
frangian <franciscogianzanti@gmail.com>
2022-12-09 00:48:55 -0300

    1era parte

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,87 @@
-import React from 'react'
-
-//Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { ContextGlobal } from "./utils/global.context";
+import { removeTokenFromStorage } from "./utils/localStorage.service";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+  const { theme, setDarkTheme, setLightTheme, login, setLogout } =
+    useContext(ContextGlobal);
+  const isDarkMode = theme === "dark" || false;
+
+  const changeTheme = () => {
+    if (isDarkMode) setLightTheme();
+    else setDarkTheme();
+  };
+
+  const logout = () => {
+    setLogout();
+    removeTokenFromStorage();
+  };
 
   return (
-    <nav>
-      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
-      {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
-    </nav>
-  )
-}
+    <header className="sticky-top">
+      <nav
+        className={`navbar navbar-expand-sm ${
+          isDarkMode ? "navbar-dark bg-dark" : "navbar-light bg-light"
+        }`}
+        aria-label="Third navbar example"
+      >
+        <div className="container">
+          <Link className={`navbar-brand ${styles.navbarBrand}`} to="/home">
+            Redmond Odonto
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarsExample03"
+            aria-controls="navbarsExample03"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-export default Navbar
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarsExample03"
+          >
+            <ul className="navbar-nav mb-2 mb-sm-0">
+              <li className={`nav-item ${styles.navBarLink}`}>
+                <Link className="nav-link" to="/home">
+                  Home
+                </Link>
+              </li>
+              <li className={`nav-item ${styles.navBarLink}`}>
+                <Link className="nav-link" to="/favs">
+                  Favs
+                </Link>
+               </li>
+               <li className={`nav-item ${styles.navBarLink}`}>
+               <Link className="nav-link" to="/contact">
+                  Contacto
+                </Link>
+               </li>
+             
+          
+          
+              <li className={`nav-item`}>
+                <button
+                  className={`btn btn-${isDarkMode ? "light" : "dark"} ${
+                    styles.btnStyle
+                  }`}
+                  onClick={changeTheme}
+                >
+                  {isDarkMode ? "☀" : "🌙"}{" "}
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
